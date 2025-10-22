34343dee9a328cb98ec1622f566590ded44ae8a9
minispiki <140969853+minispiki@users.noreply.github.com>
2025-10-22 19:03:30 +0100

    Expressions

--- maths/main.py
+++ maths/main.py
@@ -13,10 +13,37 @@
 6 / 3
 7 / 6
 
+# Bidmas
+(1 + 2) / (3 * 4)
+
 ## Force interger
 1 // 2
 6 // 2
 
 # Get remainder
 # eg
-10 % 3
+10 % 3  # Remainder of 10 / 3
+
+# Powers
+
+2**3
+10**10
+
+# Converting to binary
+# eg
+bin(3)
+# To remove the b prefix use
+format(3, "b")
+
+# Booleans
+a = 6
+b = 7
+print("1.", a == 6)
+print("2.", a == 7)
+print("3.", a == 6 and b == 7)
+print("4.", a == 7 and b == 7)
+print("5.", not a == 7 and b == 7)
+print("6.", a == 7 or b == 7)
+print("7.", a == 7 or b == 6)
+print("8.", not (a == 7 and b == 6))
+print("9.", not a == 7 and b == 6)
