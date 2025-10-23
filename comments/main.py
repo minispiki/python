b9c412609cf2dd79c75f011769560e0b582eecd0
minispiki <140969853+minispiki@users.noreply.github.com>
2025-10-23 14:20:41 +0100

    Update main.py

--- comments/main.py
+++ comments/main.py
@@ -2,4 +2,11 @@
 # Define a and set its value to int 6
 a = 6
 
-print(a) # Print A's value out to output
+print(a)  # Print A's value out to output
+
+# Subtraction
+a = a - 1  # Decrease value of a by 1
+# a-=1
+# The above also works
+
+print("The value of 'a' is now", a)
