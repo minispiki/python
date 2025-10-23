983ae7a29f7fb38405397844f254a9b74f797f83
minispiki <140969853+minispiki@users.noreply.github.com>
2025-10-23 14:46:38 +0100

    start input

---
+++ input/main.py
@@ -0,0 +1,8 @@
+# sometimes , well , always, we cant just guess information nor can we use a config file values as simple as a name
+# But, python, like c++'s cin and getline(cin, <varname>), has a function built in.
+# Its called
+input("Hello! ") # Won't be saved
+name = input("What's your name? " ) # Stored by string, proven by
+# type(name)
+
+print("Welcome,", name, "!") # , does the job but i can use +
