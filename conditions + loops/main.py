abc867d621ceed0eb797c4406a17dd79b28cbc41
minispiki <140969853+minispiki@users.noreply.github.com>
2025-10-24 09:12:39 +0100

    add else statements

--- conditions + loops/main.py
+++ conditions + loops/main.py
@@ -3,5 +3,9 @@ while True: # Loop forever
     if len(word) == 3: # is word's length 3 letters?
         print("You did it, you gave me a 3 letter word!")
         break # exit
+    elif len(word) == 0: # if condition isnt met, check this one
+        print("There is no word.")
+    elif len(word) == 1:
+        print("I want a word not a letter")
     else:
         print(word, "is not a 3 letter word; try again.")
