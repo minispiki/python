ed95a071c65efc48645a28e029564f6f9a602565
minispiki <140969853+minispiki@users.noreply.github.com>
2025-10-24 09:18:34 +0100

    Made loops

---
+++ conditions + loops/loops/main.py
@@ -0,0 +1,12 @@
+x = 1
+while x < 10: # If true, continue
+    print(x)
+    x+=1
+
+pets=["Cat","Dog","Parrot","Fish","Snake","Turtle","Rabbit"] # list
+for word in pets: # List every pet and stop
+    print(word)
+
+# The while loop above but better
+for x in range (1,10): # condition start from 1 but stop before 10
+    print (x)
