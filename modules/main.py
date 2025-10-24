81c18909b04b118d45b31de867733db7e8edcea9
minispiki <140969853+minispiki@users.noreply.github.com>
2025-10-24 09:24:51 +0100

    finish modules

--- modules/main.py
+++ modules/main.py
@@ -1,3 +1,5 @@
 from random import randint # from random lib, add ranint (decrease bloat)  Builtin lib
+import math as m # import math but name it m
 for i in range(3):
     print(randint(1,9))
+print(m.pi) # value of pi
